c56bc2afab69098a3c16de15ec7b56b0ecd2c388
Denis Memmesheimer <denismemmesheimer@gmail.com>
2021-07-17 22:50:13 +0200

    Working version of REST API parser

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,10 @@ use eposlib::lm::LanguageModel;
 use eposlib::lm;
 use rocket::serde::{Deserialize, Serialize};
 use rocket::serde::json::{Json, json, Value};
+// use rocket::serde::json::{Json, json, Value, serde_json};
 use rocket::State;
 use eposlib::cky::ParserOutput;
 use rocket::response::status::NotFound;
-
 
 
 // use rocket::tokio::sync::Mutex;
@@ -32,17 +32,41 @@ struct ParserInput {
     tags: Option<Vec<Box<str>>>,
 }
 
+impl ParserInput {
+    fn swap_words(&mut self) -> Vec<Box<str>> {
+        std::mem::replace(&mut self.words, Vec::new())
+    }
+
+    // fn swap_tags(&mut self) -> Option<Vec<Box<str>>> {
+    //     std::mem::replace(&mut self.tags, None)
+    // }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(crate = "rocket::serde")]
 struct ElisionInput {
-    p: ParserInput,
+    query: ParserInput,
     amendments: Vec<Amendment>,
 }
 
+
 #[get("/parse", format = "json", data = "<p>")]
-async fn parse(p: Json<ParserInput>, lm: &State<LanguageModel>) -> Result<Json<Vec<ParserOutput>>, NotFound<String>> {
-    match eposlib::parse_standard(&p.words, &p.tags, &lm, p.num, p.pretty) {
+async fn parse(mut p: Json<ParserInput>, lm: &State<LanguageModel>) -> Result<Json<Vec<ParserOutput>>, NotFound<String>> {
+    // serde_json::to_string(&point).unwrap()
+    info!("{}", json!(&*p));
+    match eposlib::parse_standard(p.swap_words(), &p.tags, &lm, p.num, p.pretty) {
+        Ok(parses) => { Ok(Json(parses)) }
+        Err(e) => {
+            Err(NotFound(e))
+        }
+    }
+}
+
+#[get("/elision", format = "json", data = "<e>")]
+async fn elision(mut e: Json<ElisionInput>, lm: &State<LanguageModel>) -> Result<Json<Vec<ParserOutput>>, NotFound<String>> {
+    info!("{}", json!(&*e));
+    // info!("{}", serde_json::to_string_pretty(&*e).unwrap());
+    match eposlib::parse_ellipsis(e.query.swap_words(), &e.query.tags, &lm, e.query.num, e.query.pretty, &e.amendments) {
         Ok(parses) => { Ok(Json(parses)) }
         Err(e) => {
             Err(NotFound(e))
@@ -91,7 +115,7 @@ fn rocket() -> _ {
 
     rocket::build()
         .manage(lm::load_model(&config).unwrap())
-        .mount("/", routes![parse])
+        .mount("/", routes![parse, elision])
         .register("/", catchers![not_found])
     // .launch()
     // .await;
